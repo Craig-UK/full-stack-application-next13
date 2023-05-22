a72b1c9c574361000fc2801c76a94067ec22f181
Craig-UK <craigukdev@gmail.com>
2023-05-23 00:15:51 +0100

    FEAT: Start the development of the Navigation bar.

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,8 +1,55 @@
-import React from 'react'
+"use client";
+
+import Link from "next/link";
+import Image from "next/image";
+import { useState, useEffect } from 'react';
+import { signIn, signOut, useSession, getProviders } from 'next-auth/react';
 
 const Nav = () => {
+  const isUserLoggedIn = false;
+
+  const [providers, setProviders] = useState(null);
+
   return (
-    <div>Nav</div>
+    <nav className="flex-between w-full mb-16 pt-3">
+      <Link href="/" className="flex gap-2 flex-center">
+        <Image 
+          src="/assets/images/logo.svg"
+          alt="Promptopia Logo"
+          width={30}
+          height={30}
+          className="object-contain"
+        />
+        <p className="logo_text">Promptopia</p>
+      </Link>
+
+      {/* Desktop Navigation */}
+      <div className="sm:flex hidden"> {/* Flex on smaller sized devices, else: hidden */}
+        {isUserLoggedIn ? (
+          <div className="flex gap-3 md:gap-5"> {/* Gap 5 on medium sized devices */}
+            <Link href="/create-prompt" className="black_btn">
+              Create Post
+            </Link>
+            <button type="button" onClick={signOut} className="outline_btn">
+              Sign Out
+            </button>
+            <Link href="/profile">
+              <Image 
+                src="/assets/images/logo.svg"
+                width={37}
+                height={37}
+                className="rounded-full"
+                alt="profile"
+              />
+            </Link>
+          </div>
+        ): (
+          <>
+
+          </>
+        )}
+      </div>
+    </nav>
   )
 }
 
