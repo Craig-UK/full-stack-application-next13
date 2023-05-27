14064ff28d1d6712ae9e58b6a1d999eac4276614
Craig-UK <craigukdev@gmail.com>
2023-05-28 00:07:38 +0100

    FEAT: Users are now able to search on the 'Home' page in the 'Feed' using the search bar. Users can search by the tag by either clicking the tag on a prompt, by entering a username into the search bar, or by entering text that is contained within a prompt where the prompts that contain the text entered will be returned and displayed on the feed.

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -7,7 +7,7 @@ const PromptCardList = ({ data, handleTagClick }) => {
   return (
     <div className="mt-16 prompt_layout">
       {data.map((post) => (
-        <PromptCard 
+        <PromptCard
           key={post._id}
           post={post}
           handleTagClick={handleTagClick}
@@ -18,23 +18,50 @@ const PromptCardList = ({ data, handleTagClick }) => {
 };
 
 const Feed = () => {
-  const [searchText, setSearchText] = useState('');
-  const [posts, setPosts] = useState([]);
+  const [allPosts, setAllPosts] = useState([]);
 
-  const handleSearchChange = (e) => {
+  const [searchText, setSearchText] = useState("");
+  const [searchTimeout, setSearchTimeout] = useState(null);
+  const [searchedResults, setSearchedResults] = useState([]);
 
-  }
+  const fetchPosts = async () => {
+    const response = await fetch("/api/prompt");
+    const data = await response.json();
+
+    setAllPosts(data);
+  };
 
   useEffect(() => {
-    const fetchPosts = async () => {
-      const response = await fetch('/api/prompt');
-      const data = await response.json();
-
-      setPosts(data);
-    }
-
     fetchPosts();
-  }, [])
+  }, []);
+
+  const filterPrompts = (searchText) => {
+    const regex = new RegExp(searchText, "i"); // 'i' flag for case-insensitive search
+    return allPosts.filter(
+      (item) =>
+        regex.test(item.creator.username) ||
+        regex.test(item.tag) ||
+        regex.test(item.prompt)
+    );
+  };
+
+  const handleSearchChange = (e) => {
+    clearTimeout(searchTimeout);
+    setSearchText(e.target.value);
+
+    // Debounce method
+    setSearchTimeout(() => {
+      const searchResult = filterPrompts(e.target.value);
+      setSearchedResults(searchResult);
+    }, 500);
+  };
+
+  const handleTagClick = (tagName) => {
+    setSearchText(tagName);
+
+    const searchResult = filterPrompts(tagName);
+    setSearchedResults(searchResult);
+  };
 
   return (
     <section className="feed">
@@ -49,10 +76,14 @@ const Feed = () => {
         />
       </form>
 
-      <PromptCardList 
-        data={posts}
-        handleTagClick={() => {}}
-      />
+      {/* All Prompts */}
+      {searchText ? (
+        <PromptCardList 
+          data={searchedResults} 
+          handleTagClick={handleTagClick} />
+      ): (
+        <PromptCardList data={allPosts} handleTagClick={handleTagClick} />
+      )}
     </section>
   );
 };
