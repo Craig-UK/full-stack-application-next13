65ed7e73091f617af95b14a169b8f8a86b8756dd
Craig-UK <craigukdev@gmail.com>
2023-05-29 00:55:00 +0100

    FIX: Add dynamic export to force dynamic route to attempt to fix new prompts/posts not displaying on the Home page on the Feed.

--- app/api/prompt/route.js
+++ app/api/prompt/route.js
@@ -8,12 +8,11 @@ export const GET = async (request) => {
         const prompts = await Prompt.find({}).populate('creator');
 
         return new Response(JSON.stringify(prompts), { 
-            status: 200,
-            headers: {
-                'Content-Type': 'application/json'
-            }
+            status: 200
          });
     } catch (error) {
         return new Response("Failed to fetch all prompts", { status: 500 });
     }
 }
+
+export const dynamic = 'force-dynamic';
