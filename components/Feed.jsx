9cf74bfa7b11ff94f83282af97158febc9d2f462
Craig-UK <craigukdev@gmail.com>
2023-05-28 22:24:47 +0100

    FIX: Attempt to fix new prompts/posts not displaying on the Home page on the Feed.

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -24,14 +24,13 @@ const Feed = () => {
   const [searchTimeout, setSearchTimeout] = useState(null);
   const [searchedResults, setSearchedResults] = useState([]);
 
-  const fetchPosts = async () => {
-    const response = await fetch("/api/prompt");
-    const data = await response.json();
-
-    setAllPosts(data);
-  };
-
   useEffect(() => {
+    const fetchPosts = async () => {
+      const response = await fetch("/api/prompt");
+      const data = await response.json();
+  
+      setAllPosts(data);
+    };
     fetchPosts();
   }, []);
 
