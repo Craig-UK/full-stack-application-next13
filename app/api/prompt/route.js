b7b4fb5a0b076096b29f8675f3e13066d1d48729
Craig-UK <craigukdev@gmail.com>
2023-05-29 00:50:18 +0100

    FIX: Add headers to '/api/prompt' route to attempt to fix new prompts/posts not displaying on the Home page on the Feed.

--- app/api/prompt/route.js
+++ app/api/prompt/route.js
@@ -7,7 +7,12 @@ export const GET = async (request) => {
 
         const prompts = await Prompt.find({}).populate('creator');
 
-        return new Response(JSON.stringify(prompts), { status: 200 });
+        return new Response(JSON.stringify(prompts), { 
+            status: 200,
+            headers: {
+                'Content-Type': 'application/json'
+            }
+         });
     } catch (error) {
         return new Response("Failed to fetch all prompts", { status: 500 });
     }
